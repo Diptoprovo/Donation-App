7175b117be52591a81b05891e7793a5011852815
Diptoprovo <mondaldipto1606@gmail.com>
2025-04-04 18:40:58 +0530

    updated routes

--- client2/src/App.jsx
+++ client2/src/App.jsx
@@ -1,11 +1,33 @@
+import React from "react"
+import { ToastContainer, toast } from "react-toastify"
+import NavBar from "./components/NavBar"
+import Index from "./pages/Index";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import ItemUpload from "./pages/ItemUpload";
+import Transactions from "./pages/Transactions";
+import NotFound from "./pages/NotFound";
+
+import { Route, Routes } from "react-router-dom";
 
 function App() {
 
   return (
     <>
-      <div className="text-7xl">
-        hello
+      <ToastContainer />
+      <div>
+        <NavBar />
       </div>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/item-upload" element={<ItemUpload />} />
+        <Route path="/transactions" element={<Transactions />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   )
 }
